2f7448f9abf27b68af34fdb62052f850e0b85894
Punde <nikita.punde@capgemini.com>
2023-01-04 10:29:11 +0530

    updated

---
+++ assignment_5CF_Q4/assignment_5CF_Q4/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+
+namespace assignment_5CF_Q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+            {
+
+                int capacity, ch;
+
+                Console.WriteLine("Enter Capacity of Stack :");
+
+                capacity = int.Parse(Console.ReadLine());
+
+
+
+                MyStack<string> stack = new MyStack<string>(10);
+
+
+
+                do
+
+                {
+
+                    Console.WriteLine("Eneter your Choice: 1.Push 2.Pop 3.Display 4.Exit");
+
+
+                    ch = Convert.ToInt32(Console.ReadLine());
+
+
+                    switch (ch)
+
+                    {
+
+                        case 1:
+
+                            {
+
+                                Console.WriteLine("Enter String to Push :");
+
+                                string temp = Console.ReadLine();
+
+                                int result = stack.push(temp);
+
+
+
+                                if (result != -1)
+
+                                {
+
+                                    Console.WriteLine("Element Pushed into Stack !");
+
+                                }
+
+                                else
+
+                                {
+
+                                    Console.WriteLine("Stack Overflow !");
+
+                                }
+
+                                break;
+
+                            }
+
+                        case 2:
+
+                            {
+
+                                string Result = stack.pop();
+
+                                if (Result != null)
+
+                                {
+
+                                    Console.WriteLine("Delete Element :" + Result);
+
+                                }
+
+                                else
+
+                                {
+
+                                    Console.WriteLine("Stack Underflow !");
+
+                                }
+
+                                break;
+
+                            }
+
+
+
+                        //case 3:
+
+                        //    {
+
+                        //        Console.WriteLine("Enter Position of Element to Pop:");
+
+                        //        int Position = int.Parse(Console.ReadLine());
+
+                        //        string Result = stack.peep(Position);
+
+
+
+                        //        if (Result != null)
+
+                        //        {
+
+                        //            Console.WriteLine("Element at Position" + Position + " is " + Result);
+
+                        //        }
+
+                        //        else
+
+                        //        {
+
+                        //            Console.WriteLine("Entered Element is Out of Stack Range ");
+
+                        //        }
+
+                        //        break;
+
+                        //    }
+
+                        case 3:
+
+                            {
+
+                                string[] Elements = stack.GetAllStackElements();
+
+                                Console.WriteLine("**************Stack Content **************");
+
+                                foreach (string str in Elements)
+
+                                {
+
+                                    Console.WriteLine(str);
+
+                                }
+
+                                break;
+
+                            }
+
+                        case 4:
+                            break;
+
+
+                        default:
+
+                            {
+
+                                Console.WriteLine("Invalid Choice ");
+
+                                break;
+
+                            }
+
+                    }
+
+                } while (ch < 4);
+
+            }
+        }
+    }
---
+++ asssignment 2 oop csharp/asssignment 2 oop csharp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+
+namespace asssignment_2_oop_csharp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //public void Accept()
+            Console.WriteLine("Enter Employee details:");
+            Console.WriteLine("Emp No:");
+            int eno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Emp Name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Salary:");
+            double sal = Convert.ToDouble(Console.ReadLine());
+
+            Employee emp = new Employee(eno, name, sal);
+            emp.CalculateSalary();
+            Console.WriteLine("\nThe Emp No is      : " + emp.EMPNO);
+            Console.WriteLine("\nThe Emp Name is     :   " + emp.EMPNAME);
+            Console.WriteLine("\nThe Salary is     :   " + emp.SALARY);
+            Console.WriteLine("\n_________________________________________");
+            Console.WriteLine("\nThe HRA is     :   " + emp.HRA1);
+            Console.WriteLine("\nThe TA is     :   " + emp.TA1);
+            Console.WriteLine("\nThe DA is     :   " + emp.DA1);
+            Console.WriteLine("\nThe Gross Salary is     :   " + emp.GrossSalary1);
+            Console.WriteLine("\nThe PF is     :   " + emp.PF1);
+            Console.WriteLine("\nThe TDS is     :   " + emp.TDS1);
+            Console.WriteLine("\nThe Net Salary is     :   " + emp.NetSalary1);
+
+
+
+
+            //How to access the Methods outside the class
+            //emp.DisplayEmpDetails();
+            //Console.ReadKey();
+
+
+
+        }
+    }
+}
+
+    
+
+
+
