d1108b2e72e0bb1d7dcf8207e7d0dac7b9ac89c3
Punde <nikita.punde@capgemini.com>
2023-01-08 17:02:58 +0530

    Assignment5

---
+++ Assignment_5OOP_Q5/Assignment_5OOP_Q5/Employee.cs
@@ -0,0 +1,24 @@
+﻿namespace Assignment_5OOP_Q5
+{
+    class Employee
+    {
+        public string name;
+        public Employee(string name)
+        {
+            this.name = name;
+        }
+        public override string ToString()
+        {
+            return (name);
+        }
+    }
+}
+internal class Employee
+    {
+        private string name;
+
+        public Employee(string name)
+        {
+            this.name = name;
+        }
+    }
---
+++ Assignment_5OOP_Q5/Assignment_5OOP_Q5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Assignment_5OOP_Q5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> li = new List<Employee>();
+
+            Console.WriteLine("Enter the number of Employees you want:");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the names of employee :");
+            for (int i = 0; i < size; i++)
+            {
+                string name = Console.ReadLine();
+                li.Add(new Employee(name));
+            }
+
+            Console.WriteLine("The list of employee is :");
+            foreach (Employee emp in li)
+            {
+                Console.WriteLine(emp);
+            }
+            Console.WriteLine("List Contains " + li.Count + " Employees");
+            Console.WriteLine("Enter the name of the employee you want to search");
+            string searchName = Console.ReadLine();
+            bool found = false;
+
+            foreach (Employee e in li)
+            {
+                if (e.name.Equals(searchName, StringComparison.OrdinalIgnoreCase))
+                {
+                    found = true;
+                    
+                }
+                else
+                {
+                    found = false;
+                   
+                }
+            }
+            Console.WriteLine(found == true ? "Employee found." : "No record found.");
+        }
+    }
+}
