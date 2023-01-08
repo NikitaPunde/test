72f4b3bd96c18b3170733d26c93c819abf0602a4
Punde <nikita.punde@capgemini.com>
2023-01-08 18:47:50 +0530

    assignment

---
+++ Assignment_5OOP_Q6/Assignment_5OOP_Q6/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_5OOP_Q6
+{
+    class Player
+    {
+        public string name;
+        public int age;
+        public int runsScored;
+
+        public Player(string name, int age, int runsScored)
+        {
+            this.name = name;
+            this.age = age;
+            this.runsScored = runsScored;
+        }
+    }
+}
---
+++ Assignment_5OOP_Q6/Assignment_5OOP_Q6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Assignment_5OOP_Q6
+{
+    class Progeam
+    {
+        static void Main(string[] args)
+        {
+            Player[] India = new Player[5]
+           {
+                new Player("Sachin Tendulkar", 49, 100),
+                new Player("Rohit Sharma", 35, 75),
+                new Player("Kieron Pollard", 35, 35),
+                new Player("Ishan Kishan", 24, 90),
+                new Player("Hardik Pandya", 29, 60)
+           };
+
+            Team teamList = new Team(India);
+            foreach (Player p in teamList)
+                Console.WriteLine("Name: {0} Age: {1} Run Scored: {2} ", p.name, p.age, p.runsScored);
+        }
+    }
+}
